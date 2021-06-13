2b0486f6df53f42586405449ffccdfe90c6ca9fd
cbrandt92 <84752624+cbrandt92@users.noreply.github.com>
2021-06-13 17:09:08 -0400

    java script added

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "0f6d09ba1de0d63bb6966c01fe981fe1";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+console.log(apiURL);
+axios.get(apiURL).then(displayTemperature);
