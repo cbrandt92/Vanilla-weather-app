c815fd458242842bb033c3bd83911b98872f4931
cbrandt92 <84752624+cbrandt92@users.noreply.github.com>
2021-06-15 21:12:45 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = [
-    "Sundays",
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
@@ -38,7 +38,7 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  let forecast = repsonse.data.daily;
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
